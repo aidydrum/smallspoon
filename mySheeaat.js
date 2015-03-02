7a9a4c623c2421399ba8ba724b575549c33f63b1
aidydrum <aidydrum@users.noreply.github.com>
2015-03-02 23:02:07 +0000

    4th last go

--- mySheeaat.js
+++ mySheeaat.js
@@ -1,7 +1,19 @@
 this is line 1
 this is line 2
-Theresa May is a Jijadist, according to Sky news eearlirt
-ahhhh.. it seems, sky news, must like Fox, just used some journalisetic license
-and i made a typo
-Ballixmaite
-lknaklsna
+https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+ shell git init
+
+shell git add .
+
+shell git commit -m (MASTER- dont type) 'First Commit'
+
+copy URL on 1st commit from GitHub - where repo will be pushed
+
+in cmd prompt:  git remote add origin - URL
+
+git remote -v (for verification)
+
+Then push changes - 
+
+git push origin master on 1st
+git push on 2nd
